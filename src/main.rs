25b1ba6306984e031164cba12ae23d3dba8530d8
Stephen Jayakar <aj12ay@gmail.com>
2019-01-02 23:00:30 -0600

    lol how do i fix this

--- src/main.rs
+++ src/main.rs
@@ -76,6 +76,8 @@ fn distance_to_height(dist: f32, angle: f32) -> f32 {
     return ((HEIGHT) as f32 - 50.0) / (dist * f32::cos(angle));
 }
 fn draw_rect(canvas: &mut Canvas<Window>, x: u32, height: f32, width: u32) {
+    let alpha = ((1.0 - (height / (HEIGHT as f32))) * 255.0) as u8;
+    canvas.set_draw_color(Color::RGB(alpha, alpha, alpha));
     let height = cmp::min(height as u32, HEIGHT);
     let x = x as i32;
     let y = ((HEIGHT / 2) - (height / 2)) as i32;
